1a83d27625228a20ef0d2b5c41bc06cbad9147b3
aneeshsingh <aneesh@alkye.com>
2023-04-01 10:36:15 +0530

    first push

---
+++ js/custom.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+    // const myModal = new bootstrap.Modal(document.getElementById('completedPopup'), options);
+
+    setTimeout(() => {
+        $('#completedPopup').modal('show');
+    }, 2000);
+    
+    $('.input-group-password .btn').click(function(){
+        $(this).children().toggle();
+        $(this).toggleClass('active');
+
+        if($(this).hasClass('active')){
+            $(this).siblings('input').attr('type', 'text');
+        }else{
+            $(this).siblings('input').attr('type', 'password');
+        }
+    });
+
+    $( "#contact-btn" ).click(function(event) {
+        // event.preventDefault();
+        if($('.phone-field').hasClass('is-invalid')){
+          return false
+        }
+        // else{
+        //   $( "#contact" ).submit();
+        // }
+    });
+
+    $('.btn-menu').click(function(){
+        $(this).children().toggle();
+        $('.navigation-menu').toggleClass('show');
+    })
+
+    if($('.owl-cards').length > 0){
+        $('.owl-cards').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots : true,
+            navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:2
+                },
+                600:{
+                    items:3
+                },
+                992:{
+                    items:4
+                },
+                1000:{
+                    items:5
+                }
+            }
+        })
+    }
+
+    var form = $("#step-form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) { element.before(error); },
+        // rules: {
+        //     confirm: {
+        //         equalTo: "#password"
+        //     }
+        // }
+    });
+    form.children("div").steps({
+        headerTag: "h3",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        autoFocus: true,
+        onStepChanging: function (event, currentIndex, newIndex)
+        {
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+        },
+        onFinishing: function (event, currentIndex)
+        {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function (event, currentIndex)
+        {
+            alert("Submitted!");
+        }
+    });
+
+    // prev
+    $('.actions ul li').eq(0).find('a').html('<i class="fa-solid me-2 fa-chevron-left"></i> <span>Previous</span>');
+    $('.actions ul li').eq(1).find('a').html('<span>Next</span> <i class="fa-solid ms-2 fa-chevron-right"></i>');
+    $('.actions ul li').eq(2).find('a').html('<span>Submit</span> <i class="fa-solid ms-2 fa-chevron-right"></i>');
+    $('.actions ul li a').addClass('btn btn-grad d-inline-flex align-items-center border-0 ls-3 text-uppercase');
+});
+
+if($('.form-validate').length > 0){
+    $.validator.setDefaults({
+        submitHandler: function() {
+            alert("submitted!");
+        }
+    });
+    $().ready(function() {
+        // validate the comment form when it is submitted
+        $('[type="submit"]').click(function(){
+            $(this).parents(".form-validate").validate();
+        })
+    });
+}
